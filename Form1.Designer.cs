2551e7d39ae10fff13fd1c2f5186ba2c3fcdafe2
unknown <seansain16@gmail.com>
2022-06-06 20:36:32 +0800

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.Gold;
+            this.a1.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(203, 179);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 100);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.ttt);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.Gold;
+            this.a2.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(329, 179);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(100, 100);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.ttt);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.Gold;
+            this.a3.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(450, 179);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(100, 100);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.ttt);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Gold;
+            this.b1.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(203, 307);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.ttt);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.Gold;
+            this.b2.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(329, 307);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.ttt);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.Gold;
+            this.b3.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(450, 307);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.ttt);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.Gold;
+            this.c1.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(203, 428);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(100, 100);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.ttt);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.Gold;
+            this.c2.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(329, 428);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(100, 100);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.ttt);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.Gold;
+            this.c3.Font = new System.Drawing.Font("Shoes center", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(450, 428);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(100, 100);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.ttt);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("This Night", 69.74999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(746, 114);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tic Tac Toe";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "new game";
+            this.button2.BackColor = System.Drawing.Color.SteelBlue;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(60, 587);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 38);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "New Game";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "off";
+            this.button3.BackColor = System.Drawing.Color.SteelBlue;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(277, 587);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 38);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Turn off music";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AccessibleName = "exit";
+            this.button4.BackColor = System.Drawing.Color.SteelBlue;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(518, 587);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 38);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(746, 656);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        bool turn = true;
+        int count_of_turn = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            player.URL = "Let the Living Beware.mp3";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a1.Enabled = true;
+            a2.Enabled = true;
+            a3.Enabled = true;
+            b1.Enabled = true;
+            b2.Enabled = true;
+            b3.Enabled = true;
+            c1.Enabled = true;
+            c2.Enabled = true;
+            c3.Enabled = true;
+
+            a1.Text = "";
+            a2.Text = "";
+            a3.Text = "";
+            b1.Text = "";
+            b2.Text = "";
+            b3.Text = "";
+            c1.Text = "";
+            c2.Text = "";
+            c3.Text = "";
+        }
+
+        private void ttt(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            count_of_turn++;
+
+            Winner();
+        }
+
+        private void Winner()
+        {
+            bool winner = false;
+
+            if ((a1.Text == a2.Text) && (a2.Text == a3.Text) && (!a1.Enabled))
+                winner = true;
+            if ((a1.Text == b1.Text) && (b1.Text == c1.Text) && (!a1.Enabled))
+                winner = true;
+            if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && (!a1.Enabled))
+                winner = true;
+            if ((a2.Text == b2.Text) && (b2.Text == c2.Text) && (!a2.Enabled))
+                winner = true;
+            if ((a3.Text == b3.Text) && (b3.Text == c3.Text) && (!a3.Enabled))
+                winner = true;
+            if ((a3.Text == b2.Text) && (b2.Text == c1.Text) && (!c1.Enabled))
+                winner = true;
+            if ((b1.Text == b2.Text) && (b2.Text == b3.Text) && (!b1.Enabled))
+                winner = true;
+            else if ((c1.Text == c2.Text) && (c2.Text == c3.Text) && (!c1.Enabled))
+                winner = true;
+
+            if (winner)
+            {
+
+                String champ = "";
+                if (turn)
+                    champ = "O";
+                else
+                    champ = "X";
+
+                MessageBox.Show(champ + " is a Champ!", "Gratula!");
+            }
+            else
+            {
+                if (count_of_turn == 9)
+                    MessageBox.Show("Draw!", "Awwww!");
+            }
+        }
+    }
+}
+
